b7ce747f44922a25988bd04187507aaaa33f9eba
Rick Busarow <rickbusarow@gmail.com>
2023-11-08 21:47:11 -0600

    replace symlinked files with Properties-parsed properties

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -13,6 +13,23 @@
  * limitations under the License.
  */
 
+import java.util.Properties
+
+/*
+ * Copyright (C) 2023 Rick Busarow
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 pluginManagement {
   val allowMavenLocal = providers
     .gradleProperty("kase.allow-maven-local")
@@ -44,6 +61,11 @@ dependencyResolutionManagement {
     mavenCentral()
     maven("https://plugins.gradle.org/m2/")
   }
+  versionCatalogs {
+    create("libs") {
+      from(files("../gradle/libs.versions.toml"))
+    }
+  }
 }
 
 rootProject.name = "build-logic"
@@ -54,3 +76,23 @@ include(
   ":core",
   ":module"
 )
+
+gradle.beforeProject {
+
+  if (project != rootProject) return@beforeProject
+
+  val extras = project.extra
+  file("../gradle.properties").inputStream()
+    .use { Properties().apply { load(it) } }
+    .asSequence()
+    .mapNotNull { (key, value) ->
+      when (val k = key) {
+        !is String -> null
+        extra.has(k) -> null
+        else -> (key as String) to value
+      }
+    }
+    .forEach { (key, value) ->
+      extras.set(key, value)
+    }
+}
