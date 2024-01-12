39634beadcfa8a03bf745dab7b3d1ce3863218ef
Rick Busarow <rickbusarow@gmail.com>
2024-01-12 12:19:43 -0600

    rename HasKases to HasParams

--- kase/src/main/kotlin/com/rickbusarow/kase/Kase.kt
+++ kase/src/main/kotlin/com/rickbusarow/kase/Kase.kt
@@ -15,18 +15,38 @@
 
 package com.rickbusarow.kase
 
+@Deprecated(
+  "renamed to HasParams",
+  ReplaceWith("HasParams", "com.rickbusarow.kase.HasParams")
+)
+public typealias HasKases<K> = HasParams<K>
+
 /**
- * Trait interface for a test class with default [kases]
+ * Should prevent name shadowing issues when referencing a `params` property in dynamic tests.
+ *
+ * @see DslMarker
+ */
+@DslMarker
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE, AnnotationTarget.FUNCTION)
+public annotation class ParamsScope
+
+/**
+ * Trait interface for a test class with default [params]
  *
  * @since 0.1.0
  */
-public interface HasKases<out K : Kase> {
+@ParamsScope
+public interface HasParams<out PARAM> {
   /**
    * The default kases for tests in this class.
    *
    * @since 0.1.0
    */
-  public val kases: List<K>
+  @Deprecated("renamed to params", ReplaceWith("params"))
+  public val kases: List<PARAM> get() = params
+
+  /** The default parameters for tests in this class. */
+  public val params: List<PARAM>
 }
 
 /**
@@ -107,10 +127,7 @@ public interface HasLabels {
 public interface HasDisplayName {
 
   /**
-   * The "name" of a [Kase] as it is used in a test function name. By default, this is a
-   * pipe-separated list of the label/value pairs, such as: `[label1: value1 | label2: value2]`.
-   *
-   * @see HasDisplayNames.displayNames for a list of the label/value pairs.
+   * The name of a test or test container, as it is displayed in the IDE and in reports.
    * @since 0.1.0
    */
   public val displayName: String
@@ -124,10 +141,10 @@ public interface HasDisplayName {
 public interface HasDisplayNames {
 
   /**
-   * The "names" of individual [Kase] parameters as they are used in test function names. By
-   * default, these are label/value pairs, such as: `["label1: value1", "label2: value2"]`.
+   * A "fully qualified" name for a test or test container, as it is
+   * displayed in the IDE and in reports. The first name in the list
+   * is the outermost container, and the last name is the test itself.
    *
-   * @see HasDisplayName.displayName for the full display name.
    * @since 0.1.0
    */
   public val displayNames: List<String>
